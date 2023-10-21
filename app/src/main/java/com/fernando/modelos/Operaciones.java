611a8b4c562b4fa967b42fff011c29f4eb0e2758
Laboratorio Estudiantes <laboratorio@ulatina.net>
2023-10-20 19:58:08 -0600

    init:Primer commit

---
+++ app/src/main/java/com/fernando/modelos/Operaciones.java
@@ -0,0 +1,39 @@
+package com.fernando.modelos;
+
+public class Operaciones {
+    public double calcular(int operador, double dato1, double dato2){
+        double resultado = 0;
+        switch (operador){
+            case 1:
+                resultado = sumar(dato1, dato2);
+                break;
+            case 2:
+                resultado = restar(dato1, dato2);
+                break;
+            case 3:
+                resultado = multiplicar(dato1, dato2);
+                break;
+            case 4:
+                resultado = dividir(dato1, dato2);
+                break;
+        }
+        return resultado;
+    }
+
+    private double sumar(double sumando1, double sumando2){
+        return sumando1 + sumando2;
+    }
+    private double restar(double minuendo, double sustraendo){
+        return minuendo - sustraendo;
+    }
+    private double multiplicar(double multiplicando, double multiplicador){
+        return multiplicando * multiplicador;
+    }
+    private double dividir(double dividendo, double divisor) throws ArithmeticException{
+        if (divisor == 0) {
+            throw new ArithmeticException("El divisor no puede ser 0");
+        }
+        return dividendo / divisor;
+    }
+
+}
